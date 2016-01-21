2390d26a94171d60185614e3d52dc5885d71de58
Yimeng Zhang <zym1010@gmail.com>
2016-01-20 22:43:57 -0500

    probably final for VR2014 docker

--- Docker/vr2014/build_script.sh
+++ Docker/vr2014/build_script.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+CONTAINER_NAME=leelabcnbc_docker_vr2014
+IMAGE_BUILD_NAME=leelabcnbc/vr2014_build
+
 # build image
 docker build -t leelabcnbc/vr2014_build .
 # run once to get container
-docker run --name=leelabcnbc_docker_vr2014 leelabcnbc/vr2014_build
-docker export --output="leelabcnbc_docker_vr2014.tar" leelabcnbc_docker_vr2014
+docker run --name=${CONTAINER_NAME} ${IMAGE_BUILD_NAME}
+docker export --output="${CONTAINER_NAME}.tar" ${CONTAINER_NAME}
 
 # remove all container and image
-docker rm leelabcnbc_docker_vr2014
-docker rmi vr2014_build
-
+docker rm ${CONTAINER_NAME}
+docker rmi ${IMAGE_BUILD_NAME}
 # to run it, first import it
 # docker import leelabcnbc_docker_vr2014.tar leelabcnbc/vr2014
 # since CMD is lost, now we need to specify a command.
