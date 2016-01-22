4824c1b68e36eb03fad104cc3b5826afcb94ab73
Yimeng Zhang <zym1010@gmail.com>
2016-01-21 23:37:35 -0500

    fix bug in R package dependency

--- Docker/vr2014/vr2014_figure_script_R_install_packages.R
+++ Docker/vr2014/vr2014_figure_script_R_install_packages.R
@@ -1,5 +1,5 @@
 # this is originally from http://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them
 
-list.of.packages <- c("ggplot2","Hmisc","plyr","ggthemes","R.matlab")
+list.of.packages <- c("ggplot2","Hmisc","plyr","ggthemes","R.matlab","gdata")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos='http://lib.stat.cmu.edu/R/CRAN/')
