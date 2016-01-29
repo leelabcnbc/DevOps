b97899ac5fb0ab6c993f7c2973449a0bf8dd9b43
Yimeng Zhang <zym1010@gmail.com>
2016-01-28 20:47:52 -0500

    Update build_script.sh

--- Docker/lab-website/build_script.sh
+++ Docker/lab-website/build_script.sh
@@ -10,6 +10,6 @@ docker build -t ${IMAGE_BUILD_NAME} .
 # docker run --rm -v /vagrant:/srv/jekyll -it -p 4000:4000 leelabcnbc/lab-website
 # where /vagrant has the website folder
 
-# when running, use the following to extract nodes_modules (zxvpf, not xvpf).
+# when running, use a command similar to the following to extract nodes_modules (zxvpf, not xvpf).
 # Perhaps this is due to Apline Linux's problem on tar.
-# tar zxvpf node_modules.tar.gz -C node_modules/
+# tar zxvpf ../node_modules.tar.gz -C node_modules/
