88635688a47959d94495f7b5eb3bb54f4428e415
Yimeng Zhang <zym1010@gmail.com>
2016-02-09 11:31:07 -0500

    remove build cache

--- Docker/stimulus_generation/conda_stimulus_generation.sh
+++ Docker/stimulus_generation/conda_stimulus_generation.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 conda create -y --name stimulus_generation python numpy scipy matplotlib ipython nose pip enum34 ipython-notebook pillow scikit-image
+conda clean -y --tarballs --packages  # remove all cache.
 . activate stimulus_generation
 pip install imagen==2.0.1
