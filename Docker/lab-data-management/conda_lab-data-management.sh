e466c21137eae0d644f8eb22220dd6d4d261eb2a
Yimeng Zhang <zym1010@gmail.com>
2016-02-09 10:50:09 -0500

    add Dockerfile for stimulus generation environment

--- Docker/lab-data-management/conda_lab-data-management.sh
+++ Docker/lab-data-management/conda_lab-data-management.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-conda create --name lab-data-management python nose pip enum34
+conda create -y --name lab-data-management python nose pip enum34
 . activate lab-data-management
 pip install pymongo jsonschema rfc3987 strict-rfc3339
