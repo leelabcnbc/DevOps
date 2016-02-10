d1ccb58339425bb68b4f9946fb2cad9fa6201253
Yimeng Zhang <zym1010@gmail.com>
2016-02-10 10:06:39 -0500

    Update conda_lab-data-management.sh
    to migrate to Python 3

--- Docker/lab-data-management/conda_lab-data-management.sh
+++ Docker/lab-data-management/conda_lab-data-management.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-conda create -y --name lab-data-management python nose pip enum34
+conda create -y --name lab-data-management python=3 nose pip
 . activate lab-data-management
 pip install pymongo jsonschema rfc3987 strict-rfc3339
