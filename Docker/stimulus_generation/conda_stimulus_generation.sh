f7fcb6a9e20fae3913e09f103a01073dd099f288
Yimeng Zhang <zym1010@gmail.com>
2016-02-11 21:54:55 -0500

    use stable numpy and scipy for stimulus_generation

--- Docker/stimulus_generation/conda_stimulus_generation.sh
+++ Docker/stimulus_generation/conda_stimulus_generation.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-conda create -y --name stimulus_generation nomkl python numpy scipy matplotlib ipython nose pip enum34 ipython-notebook pillow scikit-image
+conda create -y --name stimulus_generation nomkl python numpy=1.9.3 scipy= matplotlib ipython nose pip enum34 ipython-notebook pillow scikit-image
 conda clean -y --tarballs --packages  # remove all cache.
 . activate stimulus_generation
 pip install imagen==2.0.1
