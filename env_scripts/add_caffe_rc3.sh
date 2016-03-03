3c6d7a9bf46745caa46bb3950a00630b70161ebb
Yimeng Zhang <zym1010@gmail.com>
2016-03-03 12:18:00 -0500

    add env scripts and install scripts for Caffe

---
+++ env_scripts/add_caffe_rc3.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=${HOME}/miniconda2/envs/cafferc3/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=${HOME}/software/caffe-rc3/python:$PYTHONPATH
---
+++ env_scripts/add_cudnn_v3.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=${HOME}/lib/cuDNN/cudnn-v3/lib64:$LD_LIBRARY_PATH
+export LIBRARY_PATH=${HOME}/lib/cuDNN/cudnn-v3/lib64:$LIBRARY_PATH
+export CPATH=${HOME}/lib/cuDNN/cudnn-v3/include:$CPATH
