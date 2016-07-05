39d7975a9bfd5834f781475a81ba16bc212a0f49
Yimeng X Zhang <yimengzh@compute-1-5.local>
2016-07-05 17:25:29 -0400

    add some theano related scripts

--- shell_scripts/theano/install_theano_082.sh
+++ shell_scripts/theano/install_theano_082.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # install cuDNN v5 (can be done many times)
-${DIR}/../cuda/install_cudnn_v5.sh
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+${DIR}/../cuda/install_cudnn_v5.sh
 cp "${DIR}/theanorc" "${HOME}/.theanorc"
