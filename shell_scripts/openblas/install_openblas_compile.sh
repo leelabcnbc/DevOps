505d1a652b17b4a9b1596922c41861ca0f31c4e6
Yimeng Zhang <zym1010@gmail.com>
2016-07-05 14:49:46 -0400

    finish openblas compile script

--- shell_scripts/openblas/install_openblas_compile.sh
+++ shell_scripts/openblas/install_openblas_compile.sh
@@ -4,9 +4,12 @@
 # should be changed to /data2/leelab in the future, for all common, but not public files.
 SOFTWARE_ROOT_PATH="${HOME}/software"
 INSTALL_PATH="${HOME}/lib/OpenBLAS/install"
+BINARY_PATH="${HOME}/lib/OpenBLAS/result"
 rm -rf "${INSTALL_PATH}"
 mkdir -p "${INSTALL_PATH}"
-tar -xzvf "${SOFTWARE_ROOT_PATH}/openblas/OpenBLAS-0.2.18.tar.gz" -C ${INSTALL_PATH}
+rm -rf "${BINARY_PATH}"
+mkdir -p "${BINARY_PATH}"
+tar -xzvf "${SOFTWARE_ROOT_PATH}/openblas/OpenBLAS-0.2.18.tar.gz" --strip-components=1  -C ${INSTALL_PATH}
 cd "${INSTALL_PATH}"
 
 # below is basically a copy of
@@ -14,3 +17,7 @@ cd "${INSTALL_PATH}"
 CF="${CFLAGS}"
 unset CFLAGS
 make QUIET_MAKE=1 DYNAMIC_ARCH=1 BINARY=64 NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 CFLAGS="${CF}"
+make PREFIX="${BINARY_PATH}" install
+cd "${BINARY_PATH}"/..
+# create result under ~/lib/OpenBLAS
+tar -cjvf OpenBLAS-0.2.18-multithread.tar.bz2 -C "${BINARY_PATH}"  .
