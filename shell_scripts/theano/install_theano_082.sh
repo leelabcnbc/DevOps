53bf2db45c790c8b7bac82c58db71bf66b68226f
Yimeng Zhang <zym1010@gmail.com>
2016-07-07 09:18:52 -0400

    make compile dir to be local for cluster use

--- shell_scripts/theano/install_theano_082.sh
+++ shell_scripts/theano/install_theano_082.sh
@@ -3,4 +3,4 @@
 # install cuDNN v5 (can be done many times)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ${DIR}/../cuda/install_cudnn_v5.sh
-echo -e  "[cuda]\nroot = ${HOME}/cuda\n[global]\ndevice = gpu0\nfloatX = float32"  >  "${HOME}/.theanorc"
+echo -e  "[cuda]\nroot = ${HOME}/cuda\n[global]\ndevice = gpu0\nfloatX = float32\nbase_compiledir = /tmp/.theano_${USER}"  >  "${HOME}/.theanorc"
