e068c9869f573522c4a841bd33d02d54f51773b0
Yimeng Zhang <zym1010@gmail.com>
2016-07-07 09:23:26 -0400

    add reminder to define CUDA_CACHE_PATH for cluster

--- shell_scripts/cuda/install_cuda_7.5.sh
+++ shell_scripts/cuda/install_cuda_7.5.sh
@@ -14,3 +14,7 @@ sh cuda_7.5.18_linux.run --silent --toolkit --samples --no-opengl-libs \
 
 echo "CUDA 7.5 has been installed under ${TOOLKITPATH}, sample at ${SAMPLEPATH}"
 echo "create a symlink at ${HOME}/cuda for convenience, as we usually only need one single CUDA toolkit"
+
+
+# check <https://devblogs.nvidia.com/parallelforall/cuda-pro-tip-understand-fat-binaries-jit-caching/>
+echo "for performance (on cluster), add 'export CUDA_CACHE_PATH=\"/tmp/.nv_\${USER}/ComputeCache\"' under appropriate file, such as .bashrc or .profile"
