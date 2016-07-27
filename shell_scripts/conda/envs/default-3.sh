c80c8790f73bfd3ab7c67f0217c7cde8261e0bb2
Yimeng Zhang <zym1010@gmail.com>
2016-07-27 09:43:10 +0800

    bump scipy to 0.18.0

--- shell_scripts/conda/envs/default-3.sh
+++ shell_scripts/conda/envs/default-3.sh
@@ -21,5 +21,5 @@ fi
 # I need to fix versions, as well as specify channel, so that packages from
 # conda-forge will be used.
 conda create --yes --channel conda-forge --show-channel-urls -n ${ENV_NAME} \
-    python=3.5 numpy=1.11.1 scipy=0.17.1 matplotlib=1.5.2\
+    python=3.5 numpy=1.11.1 scipy=0.18.0 matplotlib=1.5.2\
     pandas=0.18.1 nose=1.3.7 notebook=4.2.1 h5py=2.6.0 openblas=0.2.18 git=2.8.2
