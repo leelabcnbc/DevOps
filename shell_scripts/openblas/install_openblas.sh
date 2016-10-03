392a25f86e33c0acc6cc227926b41af4cbfe489f
Yimeng Zhang <zym1010@gmail.com>
2016-10-02 21:18:02 -0400

    update to latest conda-forge openblas

--- shell_scripts/openblas/install_openblas.sh
+++ shell_scripts/openblas/install_openblas.sh
@@ -5,5 +5,5 @@
 SOFTWARE_ROOT_PATH="/data2/leelab/software"
 INSTALL_PATH="${HOME}/lib/OpenBLAS"
 mkdir -p ${INSTALL_PATH}
-# this is multithread version (build number 4, see https://github.com/conda-forge/openblas-feedstock/pull/9)
-tar -xjvf "${SOFTWARE_ROOT_PATH}/openblas/linux-64-openblas-0.2.18-4.tar.bz2" -C ${INSTALL_PATH}
+# latest version, to fix a potential bug <https://github.com/conda-forge/openblas-feedstock/pull/12>
+tar -xjvf "${SOFTWARE_ROOT_PATH}/openblas/linux-64-openblas-0.2.18-5.tar.bz2" -C ${INSTALL_PATH}
