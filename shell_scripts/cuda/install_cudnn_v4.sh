f31db44eec196eb0851b4fd8c943a3f05f62f694
Yimeng Zhang <zym1010@gmail.com>
2016-10-09 17:23:48 -0400

    close #13

--- shell_scripts/cuda/install_cudnn_v4.sh
+++ shell_scripts/cuda/install_cudnn_v4.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-# Install CUDA 7.5 for Linux in home directory.
-# check if the file exists.
-# should be changed to /data2/leelab in the future, for all common, but not public files.
-SOFTWARE_ROOT_PATH="/data2/leelab/software"
+# Install CUDNN v4.0 for CUDA 7.0+ for Linux in home directory.
 INSTALL_PATH="${HOME}/lib/cuDNN/v4"
 rm -rf ${INSTALL_PATH}
 mkdir -p ${INSTALL_PATH}
-tar -xzvf "${SOFTWARE_ROOT_PATH}/cudnn/cudnn-7.0-linux-x64-v4.0-prod.tgz" -C ${INSTALL_PATH}
+cd ~
+CUDNN_DOWNLOAD_SUM=4e64ef7716f20c87854b4421863328e17cce633330c319b5e13809b61a36f97d && \
+curl -fL http://developer.download.nvidia.com/compute/redist/cudnn/v4/cudnn-7.0-linux-x64-v4.0-prod.tgz -O && \
+echo "$CUDNN_DOWNLOAD_SUM  cudnn-7.0-linux-x64-v4.0-prod.tgz" | sha256sum -c - && \
+tar -xzvf "cudnn-7.0-linux-x64-v4.0-prod.tgz" -C ${INSTALL_PATH} && \
+rm ~/cudnn-7.0-linux-x64-v4.0-prod.tgz
--- shell_scripts/cuda/install_cudnn_v5.sh
+++ shell_scripts/cuda/install_cudnn_v5.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-# Install CUDA 7.5 for Linux in home directory.
-# check if the file exists.
-# should be changed to /data2/leelab in the future, for all common, but not public files.
-SOFTWARE_ROOT_PATH="/data2/leelab/software"
+# Install CUDNN v5.1 for CUDA 7.5 for Linux in home directory.
 INSTALL_PATH="${HOME}/lib/cuDNN/v5"
-# clean, since previously I installed cuDNN v5.0 before.
 rm -rf ${INSTALL_PATH}
 mkdir -p ${INSTALL_PATH}
-tar -xzvf "${SOFTWARE_ROOT_PATH}/cudnn/cudnn-7.5-linux-x64-v5.1.tgz" -C ${INSTALL_PATH}
+cd ~
+CUDNN_DOWNLOAD_SUM=40d506d0a8a00a3faccce1433346806b8cd2535683b6f08a63683ce6e474419f && \
+curl -fL http://developer.download.nvidia.com/compute/redist/cudnn/v5.1/cudnn-7.5-linux-x64-v5.1.tgz -O && \
+echo "$CUDNN_DOWNLOAD_SUM  cudnn-7.5-linux-x64-v5.1.tgz" | sha256sum -c - && \
+tar -xzvf "cudnn-7.5-linux-x64-v5.1.tgz" -C ${INSTALL_PATH} && \
+rm ~/cudnn-7.5-linux-x64-v5.1.tgz
