3a3a959e4a9ef7dd95e4a19c7879a77ded4f0a26
Yimeng Zhang <zym1010@gmail.com>
2016-11-30 17:34:16 -0500

    update pandas and openblas

--- shell_scripts/conda/envs/default-env-common.sh
+++ shell_scripts/conda/envs/default-env-common.sh
@@ -4,6 +4,11 @@ function install_default_env {
 		exit 1
 	fi
 	conda create --yes --channel conda-forge --show-channel-urls -n $1 \
-        python=$2 numpy=1.11.2 scipy=0.18.1 matplotlib=1.5.3\
-        pandas=0.19.0 nose=1.3.7 notebook=4.2.3 h5py=2.6.0 openblas=0.2.18 git=2.8.2
+        python=$2 numpy=1.11.2 scipy=0.18.1 matplotlib=1.5.3 \
+        pandas=0.19.1 nose=1.3.7 notebook=4.2.3 h5py=2.6.0 openblas=0.2.18 git=2.8.2 \
+        python-dateutil=2.5.3
+
+# dateutil version is for <https://github.com/pandas-dev/pandas/issues/14621>.
+# essentially, pandas is wrong, and we will wait for 0.19.2.
+
 }
