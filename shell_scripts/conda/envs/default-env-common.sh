09fbfe27aac7be32854304713b988ddf49330dcb
Yimeng Zhang <zym1010@gmail.com>
2016-12-04 22:40:18 -0500

    update to openblas 0.2.19

--- shell_scripts/conda/envs/default-env-common.sh
+++ shell_scripts/conda/envs/default-env-common.sh
@@ -5,12 +5,9 @@ function install_default_env {
 	fi
 	conda create --yes --channel conda-forge --show-channel-urls -n $1 \
         python=$2 numpy=1.11.2 scipy=0.18.1 matplotlib=1.5.3 \
-        pandas=0.19.1 nose=1.3.7 notebook=4.2.3 h5py=2.6.0 openblas=0.2.18 git=2.8.2 \
+        pandas=0.19.1 nose=1.3.7 notebook=4.2.3 h5py=2.6.0 openblas=0.2.19 git=2.8.2 \
         python-dateutil=2.5.3
 
 # dateutil version is for <https://github.com/pandas-dev/pandas/issues/14621>.
 # essentially, pandas is wrong, and we will wait for 0.19.2.
-
-
-# another issue is that openblas=0.2.19 doesn't have scipy 0.18.1 version. wait for them to both upgrade later.
 }
--- shell_scripts/openblas/install_openblas.sh
+++ shell_scripts/openblas/install_openblas.sh
@@ -8,6 +8,6 @@ mkdir -p ${INSTALL_PATH}
 TEMP_PATH="${HOME}/openblas-latest.tar.bz2"
 # latest version, to fix a potential bug <https://github.com/conda-forge/openblas-feedstock/pull/12>
 curl -k -L -o "${TEMP_PATH}" \
-    https://anaconda.org/conda-forge/openblas/0.2.18/download/linux-64/openblas-0.2.18-6.tar.bz2
+    https://anaconda.org/conda-forge/openblas/0.2.19/download/linux-64/openblas-0.2.19-0.tar.bz2
 tar -xjvf "${TEMP_PATH}" -C "${INSTALL_PATH}"
 rm -rf "${TEMP_PATH}"
