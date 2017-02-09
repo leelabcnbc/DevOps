ab930272fc2a4a4fcd666c6b870ccdef8d0f7b67
Yimeng Zhang <zym1010@gmail.com>
2017-02-09 14:19:08 -0500

    update conda env

--- shell_scripts/conda/envs/default-env-common.sh
+++ shell_scripts/conda/envs/default-env-common.sh
@@ -3,7 +3,8 @@ function install_default_env {
 		echo "two args, first env name, second python version"
 		exit 1
 	fi
+    # as of 02/09/2017, I updated everything to latest, except for matplotlib, whose compatibility (2.0) is not certain.
 	conda create --yes --channel conda-forge --show-channel-urls -n $1 \
-        python=$2 numpy=1.11.3 scipy=0.18.1 matplotlib=1.5.3 \
-        pandas=0.19.2 nose=1.3.7 notebook=4.3.1 h5py=2.6.0 openblas=0.2.19 git=2.8.2
+        python=$2 numpy=1.12.0 scipy=0.18.1 matplotlib=1.5.3 \
+        pandas=0.19.2 nose=1.3.7 notebook=4.3.2 h5py=2.6.0 openblas=0.2.19 git=2.8.2
 }
