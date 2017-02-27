f94ecbdbf88366796ed1dd06ee3d89d13247ab24
Yimeng Zhang <zym1010@gmail.com>
2017-02-27 12:57:01 -0500

    fix port mapping script

--- connection_scripts/slurm_create_one_port_mapping.sh
+++ connection_scripts/slurm_create_one_port_mapping.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-NOTEBOOKPORT=$(($RANDOM%63000+2001))
+NOTEBOOKPORT=$(shuf -i8888-8899 -n1)
 NOTEBOOKPORTHOST=$(hostname -i)
 ## open a tunnel between compute and login nodes
 echo -e "\n"
 echo    "  Paste ssh command in a terminal on local host (i.e., laptop)"
 echo    "  ------------------------------------------------------------"
-echo -e "  ssh -L $NOTEBOOKPORT:$NOTEBOOKPORT:$NOTEBOOKPORT $USER@$SLURM_SUBMIT_HOST\n"
+echo -e "  ssh -N -L $NOTEBOOKPORT:localhost:$NOTEBOOKPORT $USER@$SLURM_SUBMIT_HOST\n"
 echo    "  Open this address in a browser on local host; see token below"
 echo    "  ------------------------------------------------------------"
 echo -e "  localhost:$NOTEBOOKPORT                                 \n\n"
+echo -e 'In the end, run ''kill $(lsof -t -i:'"${NOTEBOOKPORT}) for potential needed cleanup\n"
 
-ssh -R $NOTEBOOKPORT:localhost:$NOTEBOOKPORT $SLURM_SUBMIT_HOST
+ssh -N -R $NOTEBOOKPORT:localhost:$NOTEBOOKPORT $SLURM_SUBMIT_HOST
