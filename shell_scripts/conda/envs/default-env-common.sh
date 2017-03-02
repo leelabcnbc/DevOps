1de09e0e0898177cf39920519d90eef1f5548844
Yimeng Zhang <zym1010@gmail.com>
2017-03-02 16:28:52 -0500

    update numpy 1.12, since now opencv is ready

--- shell_scripts/conda/envs/default-env-common.sh
+++ shell_scripts/conda/envs/default-env-common.sh
@@ -6,6 +6,6 @@ function install_default_env {
     # as of 02/09/2017, I updated everything to latest, except for matplotlib, whose compatibility (2.0) is not certain.
     # numpy hasn't been updated, since a opencv 2 linked with numpy 1.12 is not out yet.
 	conda create --yes --channel conda-forge --show-channel-urls -n $1 \
-        python=$2 numpy=1.11.3 scipy=0.18.1 matplotlib=1.5.3 \
+        python=$2 numpy=1.12.0 scipy=0.18.1 matplotlib=1.5.3 \
         pandas=0.19.2 nose=1.3.7 notebook=4.3.2 h5py=2.6.0 openblas=0.2.19 git=2.8.2
 }
