e9ff31fc977a862dd740f1d1c24a1380f81f745d
Yimeng Zhang <zym1010@gmail.com>
2017-05-08 20:52:32 -0400

    update conda and many package versions

--- shell_scripts/conda/envs/default-env-common.sh
+++ shell_scripts/conda/envs/default-env-common.sh
@@ -4,6 +4,6 @@ function install_default_env {
 		exit 1
 	fi
 	conda create --yes --channel conda-forge --show-channel-urls -n $1 \
-        python=$2 numpy=1.12.0 scipy=0.19.0 matplotlib=2.0.0 \
-        pandas=0.19.2 nose=1.3.7 notebook=4.3.2 h5py=2.6.0 openblas=0.2.19 git=2.8.2
+        python=$2 numpy=1.12.1 scipy=0.19.0 matplotlib=2.0.1 \
+        pandas=0.20.1 nose=1.3.7 notebook=5.0.0 h5py=2.7.0 openblas=0.2.19 git=2.12.2
 }
