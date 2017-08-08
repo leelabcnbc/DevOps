730b3eb8e4bb9ff2200025dc468af7164896a479
zym1010 <zym1010@gmail.com>
2017-08-08 19:40:20 -0400

    update conda and packages

--- shell_scripts/conda/envs/default-env-common.sh
+++ shell_scripts/conda/envs/default-env-common.sh
@@ -3,7 +3,10 @@ function install_default_env {
 		echo "two args, first env name, second python version"
 		exit 1
 	fi
+    # openblas 0.2.20 not ready for scipy yet. (20170808)
+    # dateutil 2.6.0 is for running test for pandas. see <https://github.com/pandas-dev/pandas/pull/16880>
 	conda create --yes --channel conda-forge --show-channel-urls -n $1 \
         python=$2 numpy=1.13.1 scipy=0.19.1 matplotlib=2.0.2 \
-        pandas=0.20.3 nose=1.3.7 pytest=3.2.0 notebook=5.0.0 h5py=2.7.0 openblas=0.2.20 git=2.13.3
+        pandas=0.20.3 nose=1.3.7 pytest=3.2.0 notebook=5.0.0 h5py=2.7.0 openblas=0.2.19 git=2.13.3 \
+        python-dateutil=2.6.0
 }
