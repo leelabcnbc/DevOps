b00ab0a35ab11b13893f865ea6bd4d3448819e25
zym1010 <zym1010@gmail.com>
2017-08-08 19:31:24 -0400

    update conda default env

--- shell_scripts/conda/envs/default-env-common.sh
+++ shell_scripts/conda/envs/default-env-common.sh
@@ -4,6 +4,6 @@ function install_default_env {
 		exit 1
 	fi
 	conda create --yes --channel conda-forge --show-channel-urls -n $1 \
-        python=$2 numpy=1.12.1 scipy=0.19.0 matplotlib=2.0.2 \
-        pandas=0.20.2 nose=1.3.7 pytest=3.0.7 notebook=5.0.0 h5py=2.7.0 openblas=0.2.19 git=2.12.2
+        python=$2 numpy=1.13.1 scipy=0.19.1 matplotlib=2.0.2 \
+        pandas=0.20.3 nose=1.3.7 pytest=3.2.0 notebook=5.0.0 h5py=2.7.0 openblas=0.2.20 git=2.13.3
 }
