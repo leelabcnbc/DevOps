28a5363d2e2f24e2c7858571c850f618450507b4
Yimeng Zhang <zym1010@gmail.com>
2017-08-08 22:19:51 -0400

    New conda env (#35) [ci skip]
    * update conda default env
    
    * update conda and packages

--- shell_scripts/conda/install_miniconda.sh
+++ shell_scripts/conda/install_miniconda.sh
@@ -25,14 +25,14 @@ fi
 
 # download a stable one, not latest, to prevent surprises.
 # follow <https://github.com/ReactionMechanismGenerator/RMG-tests/issues/18>
-INSTALLER='Miniconda2-4.1.11-Linux-x86_64.sh'
+INSTALLER='Miniconda2-4.3.21-Linux-x86_64.sh'
 cd ~
 rm -rf ~/${INSTALLER}
 wget http://repo.continuum.io/miniconda/${INSTALLER}
 # do silent installation. However, no PATH is changed.
 bash ${INSTALLER} -b -p ${INSTALL_PATH}
 # try latest one.
-${INSTALL_PATH}/bin/conda install -y conda=4.2.16
+${INSTALL_PATH}/bin/conda install -y conda=4.3.23
 # this tmux works on my account as well!
 ${INSTALL_PATH}/bin/conda install --yes --no-update-dependencies --channel conda-forge --show-channel-urls tmux=2.3
 
