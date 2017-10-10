ecdd549984b4ff847e797e981a257625b570f8b7
zym1010 <zym1010@gmail.com>
2017-10-10 14:44:26 -0400

    fix bug in conversion script

--- ansible/roles/matlab/files/convert_r2012b_cmu.sh
+++ ansible/roles/matlab/files/convert_r2012b_cmu.sh
@@ -5,14 +5,15 @@
 # mount them, and then tar.gz them, preserving permissions.
 # tested on Ubuntu 14.04.5.
 MOUNT_DIR="/tmp/matlab_iso"
+FILE="R2012b_UNIX_CMU"
 rm -rf ${MOUNT_DIR}
 mkdir ${MOUNT_DIR}
-    cp MATLAB80_common.zip MATLAB80_glnxa64.zip matlab_R2012b_glnxa64_installer.zip ${MOUNT_DIR}
-    unzip ${MOUNT_DIR}/matlab_R2012b_glnxa64_installer.zip
-    ls ${MOUNT_DIR}
-    echo "tar.gz file ${FILE}..."
-    tar -czvf ${FILE}.tar.gz -C ${MOUNT_DIR} .
-    # when untar, use tar xvpf xxx.tar.gz -C destination to keep permissions (p flag)
-    echo "done."
-done
-rmdir ${MOUNT_DIR}
+cp MATLAB80_common.zip MATLAB80_glnxa64.zip matlab_R2012b_glnxa64_installer.zip ${MOUNT_DIR}
+unzip ${MOUNT_DIR}/matlab_R2012b_glnxa64_installer.zip -d ${MOUNT_DIR}
+rm ${MOUNT_DIR}/matlab_R2012b_glnxa64_installer.zip
+ls ${MOUNT_DIR}
+echo "tar.gz file ${FILE}..."
+tar -czvf ${FILE}.tar.gz -C ${MOUNT_DIR} .
+# when untar, use tar xvpf xxx.tar.gz -C destination to keep permissions (p flag)
+rm -rf ${MOUNT_DIR}
+echo "done."
