9630260fd0e23a1f3c001b0ecc33ae6a17720380
zym1010 <zym1010@gmail.com>
2018-02-06 12:29:54 -0500

    add env script for cuda 9

---
+++ env_scripts/add_cuda_lib_v9.sh
@@ -0,0 +1,3 @@
+# this CUPTI stuff is seen in my TensorFlow scripts, so I add it here. But seems not useful though.
+export LD_LIBRARY_PATH=${HOME}/cuda-9.0/lib64:${HOME}/cuda-9.0/extras/CUPTI/lib64:$LD_LIBRARY_PATH
+export CUDA_HOME=${HOME}/cuda-9.0
